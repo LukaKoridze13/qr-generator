86610286783dbea225e856e1d7c6304449d14737
LukaKoridze13 <lukakoridze13@gmail.com>
2023-03-22 01:37:04 +0400

    Styles

--- index.js
+++
@@ -1,7 +0,0 @@
-import QRCode from './node_modules/qrcode/build/qrcode.js'
-var canvas = document.getElementById('canvas')
-
-QRCode.toCanvas(canvas, 'sample text', function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-})
